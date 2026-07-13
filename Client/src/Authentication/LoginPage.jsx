2889363f9c732297b70f3d6a01b4f1e02d159e31
Sonia Lopez <s.lopez@montpellier-bs.com>
2026-07-13 21:26:06 +0200

    LoginPage

--- Client/src/Authentication/LoginPage.jsx
+++ Client/src/Authentication/LoginPage.jsx
@@ -1,1 +1,50 @@
 import { useState } from "react";
+
+function LoginPage() {
+    const [formData, setFormData] = useState({ 
+        email : "", 
+        password : ""
+    }) 
+
+    function handleChange(event) {
+        const {name, value} = event.target; 
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault(); 
+        console.log(formData);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Email</label>
+            <input 
+            id = "email"
+            name = "email"
+            type = "email"
+            value = {formData.email}
+            onChange = {handleChange}
+            />
+
+            <label htmlFor="password">Password</label>
+            <input 
+            id = "password"
+            name = "password"
+            type = "password"
+            value = {formData.password}
+            onChange = {handleChange}
+            />
+
+            <button type= "submit">
+              Login
+            </button>
+
+        </form>
+    )
+}
+
+export default LoginPage; 

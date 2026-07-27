01acb7adaf5be2c2253f603795ac6cf318670388
Sonia Lopez <s.lopez@montpellier-bs.com>
2026-07-27 11:07:28 +0200

    Protect recipes routes

--- server/Routes/router.js
+++ server/Routes/router.js
@@ -3,11 +3,12 @@ const router = new Router();
 
 const controller = require('../Controller/recipeController')
 const authRoute = require('../Routes/authRoute'); 
+const authMiddleware = require('../Middleware/authMiddleware');
 
 router.get('/recipes', controller.getAllRecipe)
-router.post('/recipes', controller.addRecipe)
-router.delete('/recipes/:id', controller.deleteRecipe)
-router.put('/recipes/:id', controller.modifyRecipe)
+router.post('/recipes', authMiddleware, controller.addRecipe)
+router.delete('/recipes/:id', authMiddleware, controller.deleteRecipe)
+router.put('/recipes/:id', authMiddleware, controller.modifyRecipe)
 
 router.use(authRoute.routes());
 router.use(authRoute.allowedMethods());

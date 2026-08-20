a2c97f0f03d6b02c94e5d993a1b5fbfcf7dbec4c
Sonia Lopez <s.lopez@montpellier-bs.com>
2026-08-20 18:14:34 +0200

    Update Design Profile

--- Client/src/Components/NavBar/NavBar.jsx
+++ Client/src/Components/NavBar/NavBar.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Logo from '../../assests/Logo.png';
-import { FaArrowDown } from "react-icons/fa";
 import './NavBar.css'
 
 function navBar() {
@@ -23,12 +22,12 @@ function navBar() {
           <img src={Logo} alt="Logo"/>
           <div className="ProfileMenu">
             <button className='dropButton' onClick= {() => setMenuOpen((previous) => !previous)}>
-            {username} <FaArrowDown /> 
+            {username}
             </button>
             {menuOpen && (
-                <div> 
-                <button className='closeButton' onClick= {() => navigate("/MyRecipes")}> 
-                  My Recicpes <FaArrowDown />
+                <div className='DropMenu'> 
+                <button className='myRecipesButton' onClick= {() => navigate("/MyRecipes")}> 
+                  My Recipes
                   </button> 
                   <button className='logout'onClick={() => logOut()} >
                     Logout 
